40c1daa40596b030215de5a2c800f152b6fa379c
Juhasz Roland <jszrl2003@gmail.com>
2024-09-05 20:51:29 +0200

    basic dependencies

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref } from 'vue';
+
+      const name = ref('John')
+      const status = ref('Active')
+      const tasks = ref(['One', 'Two', 'Three'])
+
+      const toggleStatus = () => {
+        if(status.value === 'active'){
+        status.value = 'pending';
+
+      } else if(this.status.value === 'pending'){
+        status.value = 'inactive'
+
+      } else{
+        status.value = 'active'
+        }
+}
+</script>
+
+<template>
+  <p v-if="status === 'active'">User is active</p>
+  <p v-else-if="status === 'pending'">User is pending</p>
+  <p v-else="status === 'pending'">User is inactive</p>
+
+  <h3>Tasks:</h3>
+  <ul>
+    <li v-for="task in tasks" :key="task">{{ task }}</li>
+  </ul>
+
+  <button @click="toggleStatus">Change Status</button>
+</template>
